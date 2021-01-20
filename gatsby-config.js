6dfc718a97b83a44a5b263ccd8a96d2a2800252b
Naba <11944066+Naba0123@users.noreply.github.com>
2021-01-20 23:11:34 +0900

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,20 @@ module.exports = {
           }
         ]
       },
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-35M1YM3CHN", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
   ],
 }
